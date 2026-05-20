b058bb49b80228f13fd29dc877596a8b92dae684
Lilou Pedro <lilou.pedro@ut-capitole.fr>
2026-05-20 14:32:04 +0200

    [US-001] : Créer un compte utilisateur Fixes #1 Création classe Personne

---
+++ src/main/java/fr/utc/miage/acteurs/Personne.java
@@ -0,0 +1,47 @@
+package fr.utc.miage.acteurs;
+
+public class Personne {
+    private String nom;
+    private String prenom;
+    private String email;
+    private String password;
+
+    public Personne (String nom, String prenom, String email, String password) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
