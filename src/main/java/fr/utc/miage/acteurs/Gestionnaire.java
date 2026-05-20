bc086448d0d023e4ceba3d03c7a4037bc42e95e6
Lilou Pedro <lilou.pedro@ut-capitole.fr>
2026-05-20 15:18:50 +0200

    [US-002]: Créer un compte gestionnaire Fixes #3

---
+++ src/main/java/fr/utc/miage/acteurs/Gestionnaire.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2025 David Navarre &lt;David.Navarre at irit.fr&gt;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.utc.miage.acteurs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.utc.miage.shares.Action;
+
+public class Gestionnaire extends Personne {
+    private List<Action> portefeuilleActions;
+
+    public Gestionnaire(String nom, String prenom, String email, String motDePasse) {
+        if (nom == null || nom.isEmpty() || prenom == null || prenom.isEmpty() || 
+        email == null || email.isEmpty() || motDePasse == null || motDePasse.isEmpty()) {
+            throw new IllegalArgumentException("Les paramètres ne peuvent pas être vides.");
+        }
+
+        super(nom, prenom, email, motDePasse);
+        this.portefeuilleActions = new ArrayList<>();
+    }
+    
+    public List<Action> getPortefeuilleActions() {
+        return portefeuilleActions;
+    }
+
+    public void setPortefeuilleActions(List<Action> portefeuilleActions) {
+        if (portefeuilleActions == null) {
+            throw new IllegalArgumentException("Le portefeuille d'actions ne peut pas être null.");
+        }
+        this.portefeuilleActions = portefeuilleActions;
+    }
+}
